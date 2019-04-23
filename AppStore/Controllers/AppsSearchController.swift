3be44bea8fc0cad0d0c84fd3569902cfdecf82ce
Seth Danner <sethdanner@Seths-iMac.local>
2019-04-23 16:49:12 -0600

    Added SearchResultsCell and started setting up cell layout with image, labels, and button.

--- AppStore/Controllers/AppsSearchController.swift
+++ AppStore/Controllers/AppsSearchController.swift
@@ -8,12 +8,29 @@
 
 import UIKit
 
-class AppsSearchController: UICollectionViewController {
-
+class AppsSearchController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+    
+    fileprivate let cellID = "searchResultsCell"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        collectionView.backgroundColor = .red
+        collectionView.backgroundColor = .white
+        
+        collectionView.register(SearchResultsCell.self, forCellWithReuseIdentifier: cellID)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return .init(width: view.frame.width, height: 250)
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellID, for: indexPath)
+        return cell
     }
     
     init() {
