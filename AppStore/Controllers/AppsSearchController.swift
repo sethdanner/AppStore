265e8dedee949dc9f921f6c5106158c07454d865
Seth Danner <sethdanner@Seths-iMac.local>
2019-04-23 14:43:09 -0600

    Created Today controller, Refactored code in BaseTabBarController for simplification, added AppSearchController.

---
+++ AppStore/Controllers/AppsSearchController.swift
@@ -0,0 +1,26 @@
+//
+//  AppsSearchController.swift
+//  AppStore
+//
+//  Created by Seth Danner on 4/23/19.
+//  Copyright © 2019 Seth Danner. All rights reserved.
+//
+
+import UIKit
+
+class AppsSearchController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collectionView.backgroundColor = .red
+    }
+    
+    init() {
+        super.init(collectionViewLayout: UICollectionViewFlowLayout())
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
---
+++ AppStore/Controllers/BaseTabBarController.swift
@@ -0,0 +1,34 @@
+//
+//  BaseTabBarController.swift
+//  AppStore
+//
+//  Created by Seth Danner on 4/23/19.
+//  Copyright © 2019 Seth Danner. All rights reserved.
+//
+
+import UIKit
+
+class BaseTabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        viewControllers = [
+            createNavController(viewController: UIViewController(), title: "Today", imageName: "today_icon"),
+            createNavController(viewController: UIViewController(), title: "Apps", imageName: "apps"),
+            createNavController(viewController: AppsSearchController(), title: "Search", imageName: "search")
+        ]
+    }
+    
+    fileprivate func createNavController(viewController: UIViewController, title: String, imageName: String) -> UIViewController {
+        
+        let navController = UINavigationController(rootViewController: viewController)
+        navController.navigationBar.prefersLargeTitles = true
+        viewController.navigationItem.title = title
+        viewController.view.backgroundColor = .white
+        navController.tabBarItem.title = title
+        navController.tabBarItem.image = UIImage(named: imageName)
+        
+        return navController
+    }
+}
